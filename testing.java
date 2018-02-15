180ab69716e58458e53b1c5c543fc5f1d9b72960
Gabi Linn <gal4@pct.edu>
2018-02-15 13:00:52 -0500

    Finished

--- testing.java
+++ testing.java
@@ -1,5 +1,21 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
 public class testing {
-    private String testCase1 = "(570)991-1190";
-    private String testCase2 = "(610)99012345";
-    private String testCase3 = "(570)589-16983";
+    private String testCase1 = "5701239999";
+    private String testCase2 = "(610)9901234";
+    private String testCase3 = "570589-16983";
+
+
+    @Test
+    public void happyPath() {
+        assertTrue("Good!",Telephone.format(testCase1) == "(570)123-9999");
+    }
+    @Test
+    public void noDash(){
+        assertFalse(Telephone.format(testCase2) == "(610)990-1234");
+    }
+    @Test
+    public void noparen(){
+        assertFalse(Telephone.format(testCase3)== "570589-16983");
+    }
 }
